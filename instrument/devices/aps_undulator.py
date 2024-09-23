ba474f30c0f84a5d8fb530c42b4aeb49a0c6668a
USAXS account <usaxs@usaxscontrol.xray.aps.anl.gov>
2024-09-23 16:32:52 -0500

    fix undulator

--- instrument/devices/aps_undulator.py
+++ instrument/devices/aps_undulator.py
@@ -13,7 +13,6 @@ logger = logging.getLogger(__name__)
 logger.info(__file__)
 
 import apstools.devices
-# TODO: FIx this for 12IDE
 
-undulator = apstools.devices.ApsUndulator("ID45", name="undulator")
+undulator = apstools.devices.ApsUndulator("S12ID:USID", name="undulator")
 # undulator = apstools.devices.ApsUndulatorDual("ID45", name="undulator")
