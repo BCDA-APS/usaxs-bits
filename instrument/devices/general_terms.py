4dacfe2d466a96a002a5cb183d1c72b99433ad00
Jan Ilavsky <jilavsky@gmail.com>
2024-12-13 16:36:09 -0600

    Change TODO

--- instrument/devices/general_terms.py
+++ instrument/devices/general_terms.py
@@ -19,8 +19,7 @@ import time
 from .amplifiers import upd_controls
 from ..framework import sd
 
-#TODO : urgently resolve issue with USAXS dx in which seems ot use terms.SAXS.dx 
-# as well as terms.USAXS.DX0. This is critical!
+#TODO :  resolve issues with LAX PV database 
 
 class FlyScanParameters(Device):
     """FlyScan values"""
