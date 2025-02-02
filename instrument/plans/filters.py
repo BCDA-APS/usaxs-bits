84b2bf7f918e91afc854370cdbf5e799ef41a5ff
USAXS account <usaxs@usaxscontrol.xray.aps.anl.gov>
2025-02-02 10:50:39 -0600

    tweak TR filters

--- instrument/plans/filters.py
+++ instrument/plans/filters.py
@@ -75,9 +75,9 @@ def insertTransmissionFilters():
     set filters to reduce diode damage when measuring tranmission on guard slits etc
     """
     if monochromator.dcm.energy.position < 12.1:
-        al_filters = 3
+        al_filters = 0
     elif monochromator.dcm.energy.position < 18.1:
-        al_filters = 7
+        al_filters = 3
     else:
-        al_filters = 15
+        al_filters = 7
     yield from _insertFilters_(al_filters)
