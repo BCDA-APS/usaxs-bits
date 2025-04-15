a8faaac8707c46fff04b351c7a74ad00fd864edd
USAXS account <usaxs@usaxscontrol.xray.aps.anl.gov>
2025-04-15 13:14:40 -0500

    devices work

--- src/usaxs/devices/linkam.py
+++ src/usaxs/devices/linkam.py
@@ -6,9 +6,17 @@ import datetime
 import pathlib
 import time
 
-from apstools.devices.linkam_controllers import Linkam_T96_Device
 from bluesky import plan_stubs as bps
 from ophyd import EpicsSignalRO
+from ophyd import Component
+from ophyd import Device
+from ophyd import EpicsSignal
+from ophyd import EpicsSignalRO
+from ophyd import EpicsSignalWithRBV
+from ophyd import Signal
+
+#from apstools.devices import PVPositionerSoftDoneWithStop
+from apstools.devices import PVPositionerSoftDone
 
 SECOND = 1
 MINUTE = 60 * SECOND
@@ -17,18 +25,140 @@ DAY = 24 * HOUR
 WEEK = 7 * DAY
 
 
-# TODO: what is the point of this? Why do we need to run this before anything else?
-# write output to log file in userDir, name=MMDD-HHmm-heater-log.txt
-user_dir = EpicsSignalRO("usxLAX:userDir", name="user_dir", string=True)
 
-log_file_name = pathlib.Path(user_dir.get()) / (
-    datetime.datetime.now().strftime("%m%d-%H%M-heater-log.txt")
-)
 
 # first we need to redefine Linkam_T96_Device to add some methods we really want to use.
 # then we define the instance we are going to use here.
 
 # new device definition
+### now we can define our instance...
+
+
+"""
+Linkam temperature controllers
+This is modified version of APS tools devices code
+for new linkam TC1 version
+++++++++++++++++++++++++++++++
+
+.. autosummary::
+
+   ~Linkam_T96_Device
+"""
+
+
+
+
+#this makes temperature to automatically start heating when changed
+class T96Temperature(PVPositionerSoftDone):
+    actuate = Component(EpicsSignal, "STARTHEAT", kind="config", string=True)
+    actuate_value = "On"
+
+
+
+class Linkam_T96_Device(Device):
+    """
+    Linkam model T96 temperature controller
+    Linux ioc version
+
+    EXAMPLE::
+
+        tc1 = Linkam_T96("IOC:tc1:", name="tc1")
+    
+    to get temperature, ramprate etc:
+    linkam_tc1.temperature.position which returns the current T in C
+
+    """
+    #use linkam.temperature.position to get the value, this is positoner... 
+    controller_name = "Linkam T96"
+    temperature = Component(
+        T96Temperature,
+        "",
+        readback_pv="TEMP",
+        setpoint_pv="SETPOINT:SET",
+        tolerance=1.0,
+        kind="hinted",
+    )
+    ramprate = Component(
+        PVPositionerSoftDone,
+        "",
+        readback_pv="RAMPRATE",
+        setpoint_pv="RAMPRATE:SET",
+        tolerance=1.0,
+        kind="hinted",
+    )
+    units = Component(Signal, value="C", kind="config")
+    controller_error = Component(EpicsSignalRO, "CTRLLR:ERR", kind="omitted")
+    heater_power = Component(EpicsSignalRO, "POWER", kind="omitted")
+    lnp_mode = Component(
+        PVPositionerSoftDone,
+        "",
+        readback_pv="LNP_MODE",
+        setpoint_pv="LNP_MODE:SET",
+        #tolerance=1.0,
+        kind="omitted",
+    )
+    lnp_speed = Component(
+        PVPositionerSoftDone,
+        "",
+        readback_pv="LNP_SPEED",
+        setpoint_pv="LNP_SPEED:SET",
+        #tolerance=1.0,
+        kind="omitted",
+    )
+    pressure = Component(EpicsSignalRO, "PRESSURE", kind="omitted")
+    vacuum = Component(
+        PVPositionerSoftDone,
+        "",
+        readback_pv="VACUUM",
+        setpoint_pv="VACUUM:SET",
+        #tolerance=1.0,
+        kind="omitted",
+    )
+    humidity = Component(
+        PVPositionerSoftDone,
+        "",
+        readback_pv="HUMIDITY",
+        setpoint_pv="HUMIDITY:SET",
+        #tolerance=1.0,
+        kind="omitted",
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # temperature component is the main value
+        self.temperature.name = self.name
+
+    # these are unused or old things we are not using
+    # # #ramp_at_limit = Component(EpicsSignalRO, "rampAtLimit_RBV", kind="omitted")
+    # stage_config = Component(EpicsSignalRO, "STAGE:CONFIG", kind="omitted")
+    # status_error = Component(EpicsSignalRO, "CTRLLR:ERR", kind="omitted")
+    # vacuum = Component(EpicsSignal, "VACUUM:SET", kind="omitted")
+    # vacuum_at_limit = Component(EpicsSignalRO, "VACUUM", kind="omitted")
+    # # #vacuum_limit_readback = Component(EpicsSignalWithRBV, "vacuumLimit", kind="omitted")
+    # vacuum_status = Component(EpicsSignalRO, "STAT:VAC:CNTRL", kind="omitted")  # calc
+    # controller_config = Component(EpicsSignalRO, "CONFIG", kind="omitted")
+    # controller_status = Component(EpicsSignalRO, "STATUS", kind="omitted")
+    #humidity = Component(EpicsSignalRO, "HUMIDITY", kind="omitted")
+    # lnp_mode = Component(EpicsSignal, "LNP_MODE:SET", kind="omitted")
+    # lnp_speed = Component(EpicsSignalWithRBV, "LNP_SPEED:SET", kind="omitted")
+    # lnp_status = Component(EpicsSignalRO, "STAT:LNP:PUMPING", kind="omitted")
+    #vacuum = Component(EpicsSignalRO, "VACUUM", kind="omitted")
+
+        # alias("$(P):CTRLLR:ERR", "$(PA)$(TA):controllerError_RBV")
+        # alias("$(P):CONFIG", "$(PA)$(TA):controllerConfig_RBV")
+        # alias("$(P):STATUS", "$(PA)$(TA):controllerStatus_RBV")
+        # alias("$(P):STAGE:CONFIG", "$(PA)$(TA):stageConfig_RBV")
+        # alias("$(P):TEMP", "$(PA)$(TA):temperature_RBV")
+        # alias("$(P):STARTHEAT", "$(PA)$(TA):heating")
+        # alias("$(P):RAMPRATE:SET", "$(PA)$(TA):rampRate")
+        # alias("$(P):RAMPRATE", "$(PA)$(TA):rampRate_RBV")
+        # alias("$(P):SETPOINT:SET", "$(PA)$(TA):rampLimit")
+        # alias("$(P):SETPOINT", "$(PA)$(TA):rampLimit_RBV")
+        # alias("$(P):POWER", "$(PA)$(TA):heaterPower_RBV")
+        # alias("$(P):LNP_SPEED", "$(PA)$(TA):lnpSpeed_RBV")
+        # alias("$(P):LNP_MODE:SET", "$(PA)$(TA):lnpMode")
+        # alias("$(P):LNP_SPEED:SET", "$(PA)$(TA):lnpSpeed")
 
 
 class My_Linkam_T96_Device(Linkam_T96_Device):
@@ -74,20 +204,20 @@ class My_Linkam_T96_Device(Linkam_T96_Device):
         s = [f"{v}{k}" for k, v in db.items() if v != 0]
         return " ".join(s)
 
-    def log_it(self, text):
-        """Cheap, lazy way to add to log file.  Gotta be better way..."""
-        if not log_file_name.exists():
-            # create the file and header
-            with open(log_file_name, "w") as f:
-                f.write(f"# file: {log_file_name}\n")
-                f.write(f"# created: {datetime.datetime.now()}\n")
-                f.write(f"# from: {__file__}\n")
-        with open(log_file_name, "a") as f:
-            # write the payload
-            dt = datetime.datetime.now()
-            # ISO-8601 format time, ms precision
-            iso8601 = dt.isoformat(sep=" ", timespec="milliseconds")
-            f.write(f"{iso8601}: {text}\n")
+    # def log_it(self, text):
+    #     """Cheap, lazy way to add to log file.  Gotta be better way..."""
+    #     if not log_file_name.exists():
+    #         # create the file and header
+    #         with open(log_file_name, "w") as f:
+    #             f.write(f"# file: {log_file_name}\n")
+    #             f.write(f"# created: {datetime.datetime.now()}\n")
+    #             f.write(f"# from: {__file__}\n")
+    #     with open(log_file_name, "a") as f:
+    #         # write the payload
+    #         dt = datetime.datetime.now()
+    #         # ISO-8601 format time, ms precision
+    #         iso8601 = dt.isoformat(sep=" ", timespec="milliseconds")
+    #         f.write(f"{iso8601}: {text}\n")
 
     def linkam_report(self):
         """Report current values for selected controller."""
@@ -138,8 +268,6 @@ class My_Linkam_T96_Device(Linkam_T96_Device):
         time_expires = t0 + duration
         while time.time() < time_expires:
             yield from bps.sleep(0.5)
-        # self.log_it(f"{linkam.name} holding period ended") # TODO: do you need this?
         self.linkam_report()
 
 
-### now we can define our instance...
