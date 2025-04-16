413b8a55e0e3ea52872ad1a5b583f5b8a09e6d3c
Eric Codrea <codrea.eric@gmail.com>
2025-04-16 11:26:32 -0500

    device ok

--- src/usaxs/devices/slits.py
+++ src/usaxs/devices/slits.py
@@ -12,7 +12,7 @@ from ophyd import EpicsMotor
 from ophyd import EpicsSignal
 from ophyd import MotorBundle
 
-from ..devices.general_terms import terms
+# from ..devices.general_terms import terms
 
 # from .usaxs_motor_devices import UsaxsMotor
 # from ..utils import move_motors
@@ -89,70 +89,7 @@ class GSlitDevice(MotorBundle):
     v_step_into: float = 0.4  # 0.4mm step into the beam (blocks the beam)
     tuning_intensity_threshold: int = 500
 
-    def set_size(
-        self, *args: Any, h: Optional[float] = None, v: Optional[float] = None
-    ) -> Generator[Any, None, None]:
-        """move the slits to the specified size"""
-        if h is None:
-            raise ValueError("must define horizontal size")
-        if v is None:
-            raise ValueError("must define vertical size")
-        # move_motors(self.h_size, h, self.v_size, v)
-        yield from bps.mv(
-            self.h_size,
-            h,
-            self.v_size,
-            v,
-        )
-
-    @property
-    def h_gap_ok(self) -> bool:
-        """
-        Check if the horizontal gap is within tolerance.
-
-        Returns:
-            bool: True if the horizontal gap is within tolerance, False otherwise.
-        """
-        gap = self.outb.position - self.inb.position
-        return abs(gap - terms.SAXS.guard_h_size.get()) <= self.gap_tolerance
-
-    @property
-    def v_h_gap_ok(self) -> bool:
-        """
-        Check if the vertical gap is within tolerance.
-
-        Returns:
-            bool: True if the vertical gap is within tolerance, False otherwise.
-        """
-        gap = self.top.position - self.bot.position
-        return abs(gap - terms.SAXS.guard_v_size.get()) <= self.gap_tolerance
-
-    @property
-    def gap_ok(self) -> bool:
-        """
-        Check if both horizontal and vertical gaps are within tolerance.
-
-        Returns:
-            bool: True if both gaps are within tolerance, False otherwise.
-        """
-        return self.h_gap_ok and self.v_h_gap_ok
-
-    def process_motor_records(self) -> Generator[Any, None, None]:
-        """
-        Process motor records to update their status.
-
-        Yields:
-            Generator: A generator that yields control flow back to the caller.
-        """
-        yield from bps.mv(self.top.process_record, 1)
-        yield from bps.mv(self.outb.process_record, 1)
-        yield from bps.sleep(0.05)
-        yield from bps.mv(self.bot.process_record, 1)
-        yield from bps.mv(self.inb.process_record, 1)
-        yield from bps.sleep(0.05)
 
 
 # guard_slit = GSlitDevice("", name="guard_slit")
 # usaxs_slit = UsaxsSlitDevice("", name="usaxs_slit")
-# sd.baseline.append(guard_slit)
-# sd.baseline.append(usaxs_slit)
--- src/usaxs/misc/helper_functions.py
+++ src/usaxs/misc/helper_functions.py
@@ -10,6 +10,69 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def plan_slit_ok():
+    def set_size(
+        self, *args: Any, h: Optional[float] = None, v: Optional[float] = None
+    ) -> Generator[Any, None, None]:
+        """move the slits to the specified size"""
+        if h is None:
+            raise ValueError("must define horizontal size")
+        if v is None:
+            raise ValueError("must define vertical size")
+        # move_motors(self.h_size, h, self.v_size, v)
+        yield from bps.mv(
+            self.h_size,
+            h,
+            self.v_size,
+            v,
+        )
+
+    @property
+    def h_gap_ok(self) -> bool:
+        """
+        Check if the horizontal gap is within tolerance.
+
+        Returns:
+            bool: True if the horizontal gap is within tolerance, False otherwise.
+        """
+        gap = self.outb.position - self.inb.position
+        return abs(gap - terms.SAXS.guard_h_size.get()) <= self.gap_tolerance
+
+    @property
+    def v_h_gap_ok(self) -> bool:
+        """
+        Check if the vertical gap is within tolerance.
+
+        Returns:
+            bool: True if the vertical gap is within tolerance, False otherwise.
+        """
+        gap = self.top.position - self.bot.position
+        return abs(gap - terms.SAXS.guard_v_size.get()) <= self.gap_tolerance
+
+    @property
+    def gap_ok(self) -> bool:
+        """
+        Check if both horizontal and vertical gaps are within tolerance.
+
+        Returns:
+            bool: True if both gaps are within tolerance, False otherwise.
+        """
+        return self.h_gap_ok and self.v_h_gap_ok
+
+    def process_motor_records(self) -> Generator[Any, None, None]:
+        """
+        Process motor records to update their status.
+
+        Yields:
+            Generator: A generator that yields control flow back to the caller.
+        """
+        yield from bps.mv(self.top.process_record, 1)
+        yield from bps.mv(self.outb.process_record, 1)
+        yield from bps.sleep(0.05)
+        yield from bps.mv(self.bot.process_record, 1)
+        yield from bps.mv(self.inb.process_record, 1)
+        yield from bps.sleep(0.05)
+
 def UPDRange(self) -> int:
     """
     Get the UPD range value.
