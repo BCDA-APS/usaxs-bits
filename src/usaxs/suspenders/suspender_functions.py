cb3a0c23760e527b5eb7c905373e45cac0543897
USAXS account <usaxs@usaxscontrol.xray.aps.anl.gov>
2025-04-17 13:32:04 -0500

    added suspender function

---
+++ src/usaxs/suspenders/suspender_functions.py
@@ -0,0 +1,41 @@
+from ..devices.shutters import FE_shutter
+from ..devices.shutters import mono_shutter
+from ..devices.white_beam_ready_calc import white_beam_ready
+from .suspenders import FeedbackHandlingDuringSuspension
+from ophyd import Signal
+import bluesky.suspenders
+from .permit import BeamInHutch
+import logging
+
+logger = logging.getLogger(__name__)
+logger.info(__file__)
+
+
+def suspender_in_operations():
+
+    fb = FeedbackHandlingDuringSuspension()
+    suspender_white_beam_ready = bluesky.suspenders.SuspendBoolLow(
+        white_beam_ready.available,
+        pre_plan=fb.mono_beam_lost_plan,
+        sleep=100,  # RE sleeps _before_ calling post_plan
+        post_plan=fb.mono_beam_just_came_back_but_after_sleep_plan,
+    )
+
+
+    suspend_FE_shutter = bluesky.suspenders.SuspendFloor(FE_shutter.pss_state, 1)
+
+
+    logger.info(f"mono shutter connected = {mono_shutter.pss_state.connected}")
+    # remove comment if likely to use this suspender (issue #170)
+    # suspend_mono_shutter = bluesky.suspenders.SuspendFloor(mono_shutter.pss_state, 1)
+
+    logger.info(
+        "Defining suspend_BeamInHutch.  Add as decorator to scan plans as desired."
+    )
+    suspend_BeamInHutch = bluesky.suspenders.SuspendBoolLow(BeamInHutch)
+
+def suspender_in_sim():
+        # simulators
+    _simulated_beam_in_hutch = Signal(name="_simulated_beam_in_hutch")
+    suspend_BeamInHutch = bluesky.suspenders.SuspendBoolHigh(_simulated_beam_in_hutch)
+    suspend_FE_shutter = bluesky.suspenders.SuspendBoolHigh(_simulated_beam_in_hutch)
