c8bccfc7e85352712bd3aa966d78feb7a1355cf0
Pete R Jemian <prjemian@users.noreply.github.com>
2025-05-07 16:41:21 -0500

    MNT #26 finish the starter script

--- scripts/start-usaxs-bits.sh
+++ scripts/start-usaxs-bits.sh
@@ -3,18 +3,20 @@
 # file: start-usaxs-bits.sh
 # purpose: Start USAXS bluesky controls in an IPython session
 
-export BLUESKY_ENVIRONMENT=bits_usaxs  
-export INSTRUMENT_PACKAGE_NAME=usaxs
+export BLUESKY_ENVIRONMENT=bits_usaxs
+export STARTUP_MODULE=usaxs.startup
 export CONDA_ROOT=$(conda info --base)
 
 if [ "${CONDA_PREFIX}" == "" ]; then
+    # No environment active, activate base
     source "${CONDA_ROOT}/etc/profile.d/conda.sh"
 fi
 
 # local solution to: CondaError: Run 'conda init' before 'conda activate'
+# ref: https://www.ansiblepilot.com/articles/solving-the-conda-activation-error/
 eval "$(conda shell.bash hook)"
 
 conda activate "${BLUESKY_ENVIRONMENT}"
 
 echo "CONDA_PREFIX = '${CONDA_PREFIX}'"
-ipython -i -c "from ${INSTRUMENT_PACKAGE_NAME}.startup import *"
+ipython -i -c "from ${STARTUP_MODULE} import *"
