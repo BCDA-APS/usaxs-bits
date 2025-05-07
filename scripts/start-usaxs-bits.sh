65a9d2dc0979e2285c9844b88952adc61c21ff74
Pete R Jemian <prjemian@gmail.com>
2025-05-07 14:55:44 -0500

    ENH #26 add bash starter

---
+++ scripts/start-usaxs-bits.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# file: start-usaxs-bits.sh
+# purpose: Start USAXS bluesky controls in an IPython session
+
+export BLUESKY_ENVIRONMENT=bluesky_2025_1  # TODO
+export INSTRUMENT_PACKAGE_NAME=instrument
+export CONDA_ROOT=$(conda info --base)
+
+if [ "${CONDA_PREFIX}" == "" ]; then
+    source "${CONDA_ROOT}/etc/profile.d/conda.sh"
+fi
+
+# local solution to: CondaError: Run 'conda init' before 'conda activate'
+eval "$(conda shell.bash hook)"
+
+conda activate "${BLUESKY_ENVIRONMENT}"
+
+echo "CONDA_PREFIX = '${CONDA_PREFIX}'"
+ipython -i -c "from ${INSTRUMENT_PACKAGE_NAME}.startup import *"
