28b7965774454f8afc2e4f6ca324e1a5151aef69
Rafael Vescovi <ravescovi@gmail.com>
2025-05-21 10:23:43 -0500

    rename

--- src/usaxs/startup.py
+++ src/usaxs/startup.py
@@ -96,6 +96,6 @@ if not in_operation:
     RE(make_devices(file="shutters_sim.yml", clear=False))
 
  
-from .new_plans.mono_feedback import MONO_FEEDBACK_OFF
-from .new_plans.mono_feedback import MONO_FEEDBACK_ON
+from .rafa_plans.mono_feedback import MONO_FEEDBACK_OFF
+from .rafa_plans.mono_feedback import MONO_FEEDBACK_ON
 
