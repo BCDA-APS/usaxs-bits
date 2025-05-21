5965f5f1670983acd254c4a3d23c98622525eb91
Rafael Vescovi <ravescovi@gmail.com>
2025-05-21 16:27:14 -0500

    ad

--- src/usaxs/plans/plans_ads.py
+++ src/usaxs/plans/plans_ads.py
@@ -21,13 +21,12 @@ from usaxs.utils.setup_new_user import techniqueSubdirectory
 from usaxs.utils.user_sample_title import cleanupText
 from usaxs.utils.user_sample_title import getSampleTitle
 
-from .amplifiers import autoscale_amplifiers
-from .area_detector import areaDetectorAcquire
+from .amplifiers_plan import autoscale_amplifiers
+from .area_detector_plans import areaDetectorAcquire
 from .command_list import after_plan
 from .command_list import before_plan
-from .filter_changes import insertSaxsFilters
-from .filter_changes import insertWaxsFilters
-from .I0_controls import I0_controls
+from .filter_plans import insertSaxsFilters
+from .filter_plans import insertWaxsFilters
 from .mode_changes import mode_SAXS
 from .mode_changes import mode_WAXS
 from .mono_feedback import MONO_FEEDBACK_OFF
@@ -35,7 +34,8 @@ from .mono_feedback import MONO_FEEDBACK_ON
 from .requested_stop import IfRequestedStopBeforeNextScan
 from .sample_imaging import record_sample_image_on_demand
 from .sample_transmission import measure_SAXS_Transmission
-from .trd_controls import trd_controls
+from .I0_controls import I0_controls #fix this
+
 
 logger = logging.getLogger(__name__)
 
@@ -62,6 +62,24 @@ NOTIFY_ON_BAD_FLY_SCAN = oregistry["NOTIFY_ON_BAD_FLY_SCAN"]
 saxs_det = oregistry["saxs_det"]
 waxs_det = oregistry["waxs_det"]
 waxs_stage = oregistry["waxs_stage"]
+tune_m2rp = oregistry["tune_m2rp"]
+scaler1 = oregistry["scaler1"]
+constants = oregistry["constants"]
+user_override = oregistry["user_override"]
+saxs_stage = oregistry["saxs_stage"]
+mono_shutter = oregistry["mono_shutter"]
+usaxs_shutter = oregistry["usaxs_shutter"]
+guard_slit = oregistry["guard_slit"]
+monochromator = oregistry["monochromator"]
+terms = oregistry["terms"]
+s_stage = oregistry["s_stage"]
+usaxs_slit = oregistry["usaxs_slit"]
+user_data = oregistry["user_device"]
+scaler0 = oregistry["scaler0"]
+m_stage = oregistry["m_stage"]
+a_stage = oregistry["a_stage"]
+d_stage = oregistry["d_stage"]
+trd_controls = oregistry["trd_controls"]
 
 
 logger = logging.getLogger(__name__)
@@ -78,50 +96,6 @@ user_override.register("useDynamicTime")
 DO_NOT_STAGE_THESE_KEYS___THEY_ARE_SET_IN_EPICS = """
 #     acquire_time acquire_period num_images num_exposures
 # """.split()
-
-# # Device and plan instances from oregistry (allowed list)
-mono_shutter = oregistry["mono_shutter"]
-usaxs_shutter = oregistry["usaxs_shutter"]
-# usaxs_shutter = oregistry["usaxs_shutter"]
-# ar_start = oregistry["ar_start"]
-guard_slit = oregistry["guard_slit"]
-# lax_autosave = oregistry["lax_autosave"]
-# m_stage = oregistry["m_stage"]
-monochromator = oregistry["monochromator"]
-terms = oregistry["terms"]
-s_stage = oregistry["s_stage"]
-# saxs_det = oregistry["saxs_det"]
-# saxs_stage = oregistry["saxs_stage"]
-# struck = oregistry["struck"]
-# terms = oregistry["terms"]
-# usaxs_flyscan = oregistry["usaxs_flyscan"]
-# usaxs_q_calc = oregistry["usaxs_q_calc"]
-usaxs_slit = oregistry["usaxs_slit"]
-user_data = oregistry["user_device"]
-scaler0 = oregistry["scaler0"]
-# waxs_det = oregistry["waxs_det"]
-m_stage = oregistry["m_stage"]
-a_stage = oregistry["a_stage"]
-d_stage = oregistry["d_stage"]
-# flyscan_trajectories = oregistry["flyscan_trajectories"]
-# # Plan helpers (if available in oregistry)
-# mode_USAXS = oregistry["mode_USAXS"]
-# mode_SAXS = oregistry["mode_SAXS"]
-# mode_WAXS = oregistry["mode_WAXS"]
-# record_sample_image_on_demand = oregistry["record_sample_image_on_demand"]
-# measure_USAXS_Transmission = oregistry["measure_USAXS_Transmission"]
-# measure_SAXS_Transmission = oregistry["measure_SAXS_Transmission"]
-# insertSaxsFilters = oregistry["insertSaxsFilters"]
-# insertWaxsFilters = oregistry["insertWaxsFilters"]
-# areaDetectorAcquire = oregistry["areaDetectorAcquire"]
-# autoscale_amplifiers = oregistry["autoscale_amplifiers"]
-# I0_controls = oregistry["I0_controls"]
-# I00_controls = oregistry["I00_controls"]
-# upd_controls = oregistry["upd_controls"]
-# trd_controls = oregistry["trd_controls"]
-# scaler0 = oregistry["scaler0"]
-# scaler1 = oregistry["scaler1"]
-# constants = oregistry["constants"]
 
 
 def SAXS(
