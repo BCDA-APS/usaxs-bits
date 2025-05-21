d101223793540bdf467b112dfba986ad982efa82
Rafael Vescovi <ravescovi@gmail.com>
2025-05-21 10:25:22 -0500

    uncomment

--- src/usaxs/rafa_plans/__init__.py
+++ src/usaxs/rafa_plans/__init__.py
@@ -1,38 +1,38 @@
 # # flake8: noqa: F401
 """Bluesky plans."""
 
-# # from .axis_tuning import instrument_default_tune_ranges
-# # from .axis_tuning import update_EPICS_tuning_widths
-# # from .axis_tuning import user_defined_settings
-# # from .command_list import execute_command_list
-# # from .command_list import run_command_file
-# # from .command_list import run_python_file
-# # from .command_list import sync_order_numbers
-# from .filter_plans import insertBlackflyFilters
-# from .filter_plans import insertRadiographyFilters
-# from .filter_plans import insertSaxsFilters
-# from .filter_plans import insertScanFilters
-# from .filter_plans import insertWaxsFilters
+# from .axis_tuning import instrument_default_tune_ranges
+# from .axis_tuning import update_EPICS_tuning_widths
+# from .axis_tuning import user_defined_settings
+# from .command_list import execute_command_list
+# from .command_list import run_command_file
+# from .command_list import run_python_file
+# from .command_list import sync_order_numbers
+from .filter_plans import insertBlackflyFilters
+from .filter_plans import insertRadiographyFilters
+from .filter_plans import insertSaxsFilters
+from .filter_plans import insertScanFilters
+from .filter_plans import insertWaxsFilters
 
-# # from .mode_changes import mode_BlackFly
-# from .mode_changes import mode_Radiography
-# from .mode_changes import mode_SAXS
-# from .mode_changes import mode_USAXS
-# from .mode_changes import mode_WAXS
-# from .mode_changes import mode_OpenBeamPath
+# from .mode_changes import mode_BlackFly
+from .mode_changes import mode_Radiography
+from .mode_changes import mode_SAXS
+from .mode_changes import mode_USAXS
+from .mode_changes import mode_WAXS
+from .mode_changes import mode_OpenBeamPath
 
-# from .move_instrument import move_SAXSIn
-# from .move_instrument import move_SAXSOut
-# from .move_instrument import move_USAXSIn
-# from .move_instrument import move_USAXSOut
-# from .move_instrument import move_WAXSIn
-# from .move_instrument import move_WAXSOut
+from .move_instrument import move_SAXSIn
+from .move_instrument import move_SAXSOut
+from .move_instrument import move_USAXSIn
+from .move_instrument import move_USAXSOut
+from .move_instrument import move_WAXSIn
+from .move_instrument import move_WAXSOut
 
-# # from .resets import resets
-# # from .sample_imaging import sample_imaging
-# # from .sample_transmission import sample_transmission
-# # from .scans import scans
-# from .sim_plans import sim_count_plan
-# from .sim_plans import sim_print_plan
-# from .sim_plans import sim_rel_scan_plan
-# # from .uascan import uascan
+# from .resets import resets
+# from .sample_imaging import sample_imaging
+# from .sample_transmission import sample_transmission
+# from .scans import scans
+from .sim_plans import sim_count_plan
+from .sim_plans import sim_print_plan
+from .sim_plans import sim_rel_scan_plan
+# from .uascan import uascan
