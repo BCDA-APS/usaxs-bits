bc5f3854d4829127197fd40de54611ea6632314c
Jan Ilavsky <jilavsky@gmail.com>
2025-05-22 13:51:06 -0500

    wip

--- src/usaxs/plans/amplifiers_plan.py
+++ src/usaxs/plans/amplifiers_plan.py
@@ -43,7 +43,7 @@ upd_controls = oregistry["upd_controls"]
 usaxs_shutter = oregistry["usaxs_shutter"]
 trd_controls = oregistry["trd_controls"]
 trd = oregistry["trd"]
-user_data = oregistry["user_data"]
+user_data = oregistry["user_device"]
 
 logger = logging.getLogger(__name__)
 
